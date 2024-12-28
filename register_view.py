8e633568eb37d03a28c5ed39bb8d3affab79b08b
Texdarkstar <texdarkstar@fastmail.fm>
2024-12-28 13:59:41 -0600

    added missing register_view.py

---
+++ register_view.py
@@ -0,0 +1,31 @@
+from ui import *
+from discord import Interaction, SelectOption
+from discord.ui import Select, View, Button
+from secret import *
+
+
+class SubmitButton(Button):
+    def __init__(self, fleetmanager):
+        self.fleetmanager = fleetmanager
+
+        super().__init__(label="Register")
+
+
+    async def callback(self, interaction: Interaction):
+        self.fleetmanager.update_shipyards()
+        self.fleetmanager.update_ship_models()
+        self.fleetmanager.update_ships()
+
+        await self.fleetmanager.update_user_ids()
+
+        self.view.stop()
+
+
+class RegisterView(View):
+    def __init__(self, fleetmanager, timeout=180):
+        super().__init__(timeout=timeout)
+        self.fleetmanager = fleetmanager
+
+        self.add_item(ModelDropdown(self.fleetmanager))
+        self.add_item(ShipyardDropdown(self.fleetmanager))
+        self.add_item(SubmitButton(self.fleetmanager))
